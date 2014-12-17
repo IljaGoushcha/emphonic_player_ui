6e226f0e295fed682a486b7588001d72ca762415
IljaGoushcha <iljagou@yahoo.com>
2014-12-17 11:45:24 -0500

    changed upload to Angular way

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
 angular.module('EmphonicPlayer', []);
 
-angular.module('EmphonicPlayer').controller('MainCtrl', function($scope, $q) {
+angular.module('EmphonicPlayer').controller('MainCtrl', function($scope, $http) {
     'use strict';
 
-    var key = "";
-
     $scope.getAmazonURL = function() {
-        // $http.get('http://localhost:3000/amazon/sign_key').success(function(response){
-        //     signKeyResults = response;
-        //     debugger;
-        // });
+        $http.get('http://localhost:3000/amazon/sign_key').success(function(response){
+            $scope.response = response;
+            // debugger;
+        });
 
-        console.log("hi");
-        $.ajax({
-            url: 'http://localhost:3000/amazon/sign_key',
-            type: 'GET',
-            data: {file_name: 'url.jpg'}
-        })
-        .done(function(result) {
-            // $scope.policy = result.policy;
-            $('#uploadPolicy').val(result.policy);
-            $('#uploadSignature').val(result.signature);
-            $('#accessKey').val(result.access_key);
-            $('#acl').val(result.acl);
-            $('#key').val(result.key);
-            key = "https://s3.amazonaws.com/emphonic-player-demo/" + result.key;
-            console.log(key);
-        })
-        .fail(function(error) {
-            console.log(error);
-            console.log("error");
-        })
-          .always(function() {
-            console.log("complete");
-          });
+        // console.log("hi");
+        // $.ajax({
+        //     url: 'http://localhost:3000/amazon/sign_key',
+        //     type: 'GET',
+        //     data: {file_name: 'url.jpg'}
+        // })
+        // .done(function(result) {
+        //     $scope.response = result;
+        //     $('#uploadPolicy').val(result.policy);
+        //     $('#uploadSignature').val(result.signature);
+        //     $('#accessKey').val(result.access_key);
+        //     $('#acl').val(result.acl);
+        //     $('#key').val(result.key);
+        //     key = "https://s3.amazonaws.com/emphonic-player-demo/" + result.key;
+        //     console.log(key);
+        // })
+        // .fail(function(error) {
+        //     console.log(error);
+        //     console.log("error");
+        // })
+        //   .always(function() {
+        //     console.log("complete");
+        // });
     };
 });
 

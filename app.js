c90500d70d0eba55154f97d773bba2a00ceb614b
IljaGoushcha <iljagou@yahoo.com>
2015-04-14 12:53:49 -0400

    successfully posted to S3 and psql using all angular and modal

--- app.js
+++ app.js
@@ -52,65 +52,30 @@ angular.module('EmphonicPlayer').controller('MainCtrl', function($scope, $http, 
         console.log(x);
     };
 
-    $scope.getAmazonURL = function() {
-        console.log("inside getAmazonURL()");
-        var promise = $http.get('http://localhost:3000/amazon/sign_key')
-        .success(function(response){
-            // $scope.response = response;
-            console.log(response);
-            // debugger;
-        }).error(function(data, status, headers, config){
-            console.log(data);
-            console.log(status);
-            console.log("error");
-        });
-        return promise;
-    };
-
     $scope.uploadToS3 = function() {
         console.log("inside uploadToS3()");
         var amazonSignKey = {};
+        var file = $scope.myAudioFile;
+        var title = $scope.song.title;
+        var author = $scope.song.author;
+        var album = $scope.song.album;
 
-        var makePromiseWithAmazon = function() {
-            console.log("inside makePromiseWithAmazon");
+        var makePromiseWithRailsAPI = function() {
+            console.log("inside makePromiseWithRailsAPI");
             // This service's function returns a promise, but we'll deal with that shortly
             AmazonService.getKey()
                 // then() called when son gets back
                 .then(function(data) {
                     amazonSignKey = data;
                     console.log(data);
-                    sendToS3();
+                    AmazonService.sendToS3(amazonSignKey, file, title, author, album);
                 }, function(error) {
                     // promise rejected, could log the error with: console.log('error', error);
                     console.log(error);
                 });
         };
 
-        var sendToS3 = function() {
-            console.log("inside sendToS3");
-            var formData = new FormData();
-            formData.append('acl', amazonSignKey.acl);
-            formData.append('key', amazonSignKey.key);
-            formData.append('AWSAccessKeyId', amazonSignKey.access_key);
-            formData.append('Policy', amazonSignKey.policy);
-            formData.append('Signature', amazonSignKey.signature);
-            formData.append('file', $scope.myAudioFile);
-
-            console.log("loading file to s3...");
-
-            $http.post('http://emphonic-player-demo.s3.amazonaws.com/', formData, {
-                transformRequest: angular.identity,
-                headers: {'Content-Type': undefined}
-            })
-            .success(function(){
-                console.log("file uploaded to S3 successfully");
-            })
-            .error(function(){
-                console.log("some error occured while uploading to S3");
-            });
-        };
-
-        makePromiseWithAmazon();
+        makePromiseWithRailsAPI();
     }
 
 });
--- factories/AmazonService.js
+++ factories/AmazonService.js
@@ -16,6 +16,48 @@ angular.module('EmphonicPlayer').factory('AmazonService', function ($http, $q) {
                 // something went wrong
                 return $q.reject(response.data);
             });
-        }
+        },
+        sendToS3: function(amazonSignKey, file, title, author, album) {
+            console.log("inside sendToS3");
+            var formData = new FormData();
+            formData.append('acl', amazonSignKey.acl);
+            formData.append('key', amazonSignKey.key);
+            formData.append('AWSAccessKeyId', amazonSignKey.access_key);
+            formData.append('Policy', amazonSignKey.policy);
+            formData.append('Signature', amazonSignKey.signature);
+            formData.append('file', file);
+
+            console.log("loading file to s3...");
+
+            $http.post('http://emphonic-player-demo.s3.amazonaws.com/', formData, {
+                transformRequest: angular.identity,
+                headers: {'Content-Type': undefined}
+            })
+            .success(function(){
+                console.log("file uploaded to S3 successfully");
+                var psqlData = {
+                    song: {
+                      "url": amazonSignKey.key,
+                      "title": title,
+                      "author": author,
+                      "album": album,
+                      "pitch": "0",
+                      "volume": "100",
+                      "fade_start_time": "0",
+                      "fade_stop_time": "0"
+                    }
+                }
+                $http.post('http://localhost:3000/songs', psqlData)
+                .success(function(){
+                    console.log("info posted to psql successfully");
+                })
+                .error(function(){
+                    console.log("some error occured while posting to psql");
+                });
+            })
+            .error(function(){
+                console.log("some error occured while uploading to S3");
+            });
+        },
     };
 });
